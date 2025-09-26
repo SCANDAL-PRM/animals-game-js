4970639a2dc2b9d59eaa42f792a229fb6e9f1ed9
Alex <krasaleks95@gmail.com>
2025-09-26 16:23:34 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ const cards = document.querySelectorAll(".card");
 const player = document.querySelector(".player");
 
 cards.forEach((card) => {
-  let width = card.offsetWidth;
-  let height = card.offsetHeight;
-
   card.addEventListener("click", (el) => {
-    let sound = el.target.parentElement.dataset.sound;
-    console.log(sound)
-    player.setAttribute('src', `./mp3/${sound}.mp3`)
-    player.play()
+      let sound = el.target.parentElement.dataset.sound;
+      player.setAttribute("src", `./mp3/${sound}.mp3`);
+      player.play();
   });
-
 });
